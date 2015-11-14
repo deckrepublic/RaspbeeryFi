f9a762d3b6d743af6e43f40e477d4d86093f40e6
nick <nick.a.lowery@gmail.com>
2015-11-14 10:27:20 -0700

    Clean up test.py

--- mitmscripts/test.py
+++ mitmscripts/test.py
@@ -2,20 +2,32 @@ from HTMLParser import HTMLParser
 from libmproxy.models import decoded
 
 
-injected = """
+injected = ''' 
 <script>
     alert("Fuck you.");
 </script>
-"""
+'''
+
+
+def response(context, flow):
+    with decoded(flow.response):
+        lineno, line_offset = find_injection_offset(flow.response.content) 
+        if lineno is not None and line_offset is not None:
+            flow.response.content = inject(flow.response.content, injected,
+                lineno, line_offset)
 
 
 def debug(s):
     with open('out.txt', 'a') as file:
-        file.write(str(s))
+        file.write(str(s) + '\n')
+
+
+def inject(string, injected, lineno, line_offset):
+    true_offset = find_true_offset(string, lineno, line_offset)
+    return string[:true_offset] + injected + string[true_offset:]
 
 
 def find_true_offset(string, lineno, line_offset):
-    debug(' lineno:' + str(lineno) + ', line_offset:' + str(line_offset) + ' ')
     if lineno > 1:
         newline_count = 1
         for i, c in enumerate(string):
@@ -26,20 +38,6 @@ def find_true_offset(string, lineno, line_offset):
 
     return line_offset 
 
-
-def response(context, flow):
-    def inject(lineno, offset):
-        debug('1')
-        true_offset = find_true_offset(flow.response.content, lineno, offset)
-        debug('2')
-        flow.response.content = (flow.response.content[:true_offset] +
-            injected + flow.response.content[true_offset:])
-        
-    with decoded(flow.response):
-        lineno, offset = find_injection_offset(flow.response.content) 
-        if lineno is not None and offset is not None:
-            inject(lineno, offset)
-        
 
 def find_injection_offset(html):
     thing = Empty()
@@ -57,10 +55,13 @@ def find_injection_offset(html):
 class OurHTMLParser(HTMLParser):
     def handle_starttag(self, tag, attrs):
         if tag == 'html':
-            lineno, offset = self.getpos()
-            offset += len(self.get_starttag_text())
-            self.callback(lineno, offset)
+            self.invoke_tag_callback()
 
+    def invoke_tag_callback(self):
+        lineno, offset = self.getpos()
+        offset += len(self.get_starttag_text())
+        self.callback(lineno, offset)
+        
 
 class Empty:
     pass
