2e1ec17efbd21fa3c3746e242fa36bbfee3c69ee
nick <nick.a.lowery@gmail.com>
2015-11-14 14:10:51 -0700

    Remove unused stuff

--- clients.py
+++ clients.py
@@ -1,9 +1,8 @@
 from netlib.http import Headers
 
 
-LOGIN_PAGE_PATH = 'html/login.html'
 _clients = []
-_client_flags = []
+_client_connect_flags = []
 
 
 def add_client(host):
@@ -21,24 +20,13 @@ def has_login_info(response):
 
 
 def set_client_connect_flag(host):
-    if host not in _client_flags:
-        _client_flags.append(host)
+    if host not in _client_connect_flags:
+        _client_connect_flags.append(host)
 
 
 def unset_client_connect_flag(host):
-    _client_flags.remove(host)
+    _client_connect_flags.remove(host)
 
 
 def did_client_just_connect(host):
-    return host in _client_flags
-
-
-def get_login_page_html():
-    with open(LOGIN_PAGE_PATH, 'r') as file:
-        html = file.read()
-    return html
-
-
-def replace_response_with_login_page(response):
-    response.headers = Headers(content_type='text/html')
-    response.content = get_login_page_html()
+    return host in _client_connect_flags
