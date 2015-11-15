739485dca182b17bafafcff935a7f93e0c4ec483
nick <nick.a.lowery@gmail.com>
2015-11-15 14:51:42 -0700

    Cleanup

--- clients.py
+++ clients.py
@@ -2,32 +2,79 @@ from netlib.http import Headers
 from utility import *
 
 
-_clients = []
-destinations = dict()
+LOCALHOST = 'localhost'
+_clients = dict()
+_destinations = dict()
 
 
-def add_client(host, username, password):
+def handle_request_if_new_client(flow):
+    if is_new_client(flow.client_conn.address.host):
+        handle_new_client_request(flow)
+
+
+def handle_new_client_request(flow):
+    if request_is_to_internet(flow.request):
+        remember_original_request(flow)
+        change_request_to_login_page(flow.request)
+
+    elif request_path_is_empty(flow.request):
+        change_request_to_login_page(flow.request)
+
+    elif request_has_client_login_info(flow.request):
+        get_and_store_client_login_info(flow)
+        change_request_to_original_one(flow)
+
+
+def request_is_to_internet(request):
+    return not request_is_to_localhost(request)
+
+
+def request_is_to_localhost(request):
+    return request.host == LOCALHOST
+
+
+def request_path_is_empty(request):
+    return not request.path or request.path == '/'
+
+
+def remember_original_request(flow):
+    if flow.client_conn.address.host not in _destinations:
+        _destinations[flow.client_conn.address.host] = [flow.request.host,
+            flow.request.path]
+    
+
+def store_client_login_info(host, username, password):
     if is_new_client(host):
-        _clients.append(Client(host, username, password))
-        debug(str(_clients[-1]) + '\n')
+        _clients[host] = Client(host, username, password)
+        debug(_clients[host])
 
 
 def is_new_client(host):
-    for client in _clients:
-        if client.host == host:
-            return False
-    return True
+    return host not in _clients
 
 
-def has_login_info(request):
-    return request.host == 'localhost' and request.path == '/index.html'
+def request_has_client_login_info(request):
+    return request_is_to_localhost(request) and request.path == '/index.html'
+
+
+def get_and_store_client_login_info(flow):
+    params = url_params_to_dict(flow.request.content)
+    store_client_login_info(flow.client_conn.address.host, params['login'], 
+        params['password'])
 
 
 def change_request_to_login_page(request):
-    request.host = 'localhost'
+    request.host = LOCALHOST 
     request.path = '/login.html'
     request.method = 'GET'
     request.scheme = 'http'
+
+
+def change_request_to_original_one(flow):
+    flow.request.host = _destinations[flow.client_conn.address.host][0]
+    flow.request.path = _destinations[flow.client_conn.address.host][1]
+    flow.request.method = 'GET'
+    del _destinations[flow.client_conn.address.host]
 
 
 class Client:
@@ -37,4 +84,4 @@ class Client:
         self.password = password
 
     def __str__(self):
-        return self.host + " - " + self.username + ":" + self.password
+        return self.username + ":" + self.password + '@' + self.host
--- raspberrypifi.py
+++ raspberrypifi.py
@@ -1,23 +1,6 @@
-from libmproxy.models import decoded
 from clients import *
 from utility import *
 
 
 def request(context, flow):
-    if is_new_client(flow.client_conn.address.host):
-        if flow.request.host != 'localhost':
-            if flow.client_conn.address.host not in destinations:
-                destinations[flow.client_conn.address.host] = [flow.request.host,
-                    flow.request.path]
-            change_request_to_login_page(flow.request)
-        elif flow.request.host == 'localhost' and (not flow.request.path or 
-            flow.request.path == '/'):
-            change_request_to_login_page(flow.request)
-        elif has_login_info(flow.request):
-            params = url_params_to_dict(flow.request.content)
-            add_client(flow.client_conn.address.host, params['login'],
-                params['password'])
-            flow.request.host = destinations[flow.client_conn.address.host][0]
-            flow.request.path = destinations[flow.client_conn.address.host][1]
-            flow.request.method = 'GET'
-            del destinations[flow.client_conn.address.host]
+    handle_request_if_new_client(flow)
