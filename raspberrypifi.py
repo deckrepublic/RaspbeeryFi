7976ef30851a8345d8c2159167c19149f9b3ca1d
nick <nick.a.lowery@gmail.com>
2015-11-16 09:52:40 -0700

    Missing commit file

--- raspberrypifi.py
+++ raspberrypifi.py
@@ -3,8 +3,10 @@ from utility import *
 
 
 def request(context, flow):
-    handle_request_if_new_client(flow)
-    replace_index_request_if_necessary(flow.request)
+    if is_new_client(flow.client_conn.address.host):
+        handle_new_client_request(flow)
+    else:
+        replace_localhost_request_if_necessary(flow.request)
 
 
 def response(context, flow):
